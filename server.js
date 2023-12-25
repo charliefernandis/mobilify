53d958c181f04198983e6d6c87863e3a7b852232
charliefernandis <charliefernandis28@gmail.com>
2023-12-25 14:56:55 +0530

    25-12-2023 commit-3

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use(cors())
 // So, when you see app.use(cors()) in a server's code (like in an Express.js application), it's like the server saying, "Sure, any website can ask me for information, and I'll allow it."
 
 
-app.listen(8005 , (req , res)=>{
+app.listen(PORT || 8005 , (req , res)=>{
     console.log("server running on port 8005");
 })
